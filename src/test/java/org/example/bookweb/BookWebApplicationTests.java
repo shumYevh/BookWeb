bd3398beb29dd95d13420de06a5e6098608ce386
shumYevh <shymeiko.zenya@gmail.com>
2024-04-08 16:26:40 +0300

    first commit

---
+++ src/test/java/org/example/bookweb/BookWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

3f438788903a8adeaa82b47d8ed457015e851bef
shumYevh <shymeiko.zenya@gmail.com>
2024-04-19 16:03:06 +0300

    NotBlank

--- src/main/java/org/example/bookweb/dto/CreateBookRequestDto.java
+++ src/main/java/org/example/bookweb/dto/CreateBookRequestDto.java
@@ -1,23 +1,24 @@
 package org.example.bookweb.dto;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import org.example.bookweb.validation.Isbn;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
     @Isbn
-    @NotNull
+    @NotBlank
     private String isbn;
     @NotNull
-    @Min(0)
+    @Positive
     private Double price;
     private String description;
-    @NotNull
+    @NotBlank
     private String coverImage;
 }

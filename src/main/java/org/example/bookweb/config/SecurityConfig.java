350dd24875dc5b2cdf23cd6b9f025d69c37a47cc
shumYevh <shymeiko.zenya@gmail.com>
2024-05-03 17:27:41 +0300

    fixedAcsessToSwaggerDoc

--- src/main/java/org/example/bookweb/config/SecurityConfig.java
+++ src/main/java/org/example/bookweb/config/SecurityConfig.java
@@ -34,7 +34,9 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/api/auth/**", "/swagger-ui/**")
+                                .requestMatchers("/api/auth/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()

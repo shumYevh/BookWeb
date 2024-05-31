3900318297b5fe9494e61d5144c25d64b23f265f
shumYevh <shymeiko.zenya@gmail.com>
2024-05-31 19:17:30 +0300

    implementedDocker

--- src/test/java/org/example/bookweb/BookWebApplicationTests.java
+++ src/test/java/org/example/bookweb/BookWebApplicationTests.java
@@ -1,13 +1,7 @@
 package org.example.bookweb;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookWebApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
